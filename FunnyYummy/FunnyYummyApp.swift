8734564f6ff1ae533d9b453c46be8e8d95e5cf03
Pavel Gri <zlibabaika@gmail.com>
2023-08-27 18:24:42 +0400

    Initial Commit

---
+++ FunnyYummy/FunnyYummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyYummyApp.swift
+//  FunnyYummy
+//
+//  Created by Павел Грицков on 27.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FunnyYummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
