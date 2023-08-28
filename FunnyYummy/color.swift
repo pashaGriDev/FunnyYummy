ae97edd0e0e091f0b4880137796d5bb41a7946b1
n1K1Ta00 <aleks.avers12@mail.ru>
2023-08-28 15:14:53 +0300

    add color

---
+++ FunnyYummy/color.swift
@@ -0,0 +1,15 @@
+//
+//  color.swift
+//  FunnyYummy
+//
+//  Created by Никита Мартьянов on 28.08.23.
+//
+
+import SwiftUI
+
+struct color {
+    static let buttonColor = Color("buttonColor")
+    static let backgrondColor = Color("backgrondColor")
+    static let blackTextColor = Color("blackTextColor")
+    static let colomnIngredients = Color("colomnIngredients")
+}
