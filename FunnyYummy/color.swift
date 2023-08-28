ab741306d54b24a11b8cb503c89fde27c97663a5
n1K1Ta00 <140900062+n1K1Ta00@users.noreply.github.com>
2023-08-28 17:23:15 +0300

    Update color.swift

--- FunnyYummy/color.swift
+++ FunnyYummy/color.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import Foundation
 
-struct color {
-    static let buttonColor = Color("buttonColor")
-    static let backgrondColor = Color("backgrondColor")
-    static let blackTextColor = Color("blackTextColor")
-    static let colomnIngredients = Color("colomnIngredients")
+extension Color {
+    static var buttonColor : Color {
+        Color(red: 226, green: 62, blue: 62,opacity: 1)
+    }
+    static var backgrondColor : Color {
+        Color(red: 255, green: 255, blue: 255,opacity: 1)
+    }
+    static var blackTextColor : Color {
+        Color(red: 24, green: 24, blue: 24,opacity: 1)
+    }
+    static var colomnIngredients : Color {
+        Color(red: 241, green: 241, blue: 241,opacity: 1)
+    }
 }
