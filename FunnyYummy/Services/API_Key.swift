83d5f7e62c50433cf397d7f941c640c327c38335
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-09-10 17:00:35 +0400

    update

--- FunnyYummy/Services/API_Key.swift
+++ FunnyYummy/Services/API_Key.swift
@@ -7,10 +7,7 @@
 
 import Foundation
 
-//let apiKey = "&apiKey=a4d70218c1734345b26ed04b2e50e6c5"
-//let apiKey = "&apiKey=ec302cd3ae2e439b9558cc79d26c5efa"
-//let apiKey = "&apiKey=8fb252dbc5db455aa22b7c3f5d0a952b"
-//let apiKey = "&apiKey=7a22eb83e0dd4fe992d10e99ed90eb11"
-//let apiKey = "&apiKey=98a008d46b8a42bd811cfc7b0680c3f5"
-let apiKey = "&apiKey=ece93a7c094d40aea1db05c4b0a31a41"
+let apiKey = "&apiKey=a4d70218c1734345b26ed04b2e50e6c5"
+
+
 
